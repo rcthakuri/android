8ca4a101bedd7bea422325d334340bdb7b7d19fa
rcthakuri <thakuri.ram12@gmail.com>
2022-01-08 14:14:35 +0545

    Update py_module.py
    Testing further for conflict

--- py_module.py
+++ py_module.py
@@ -2,7 +2,7 @@ import ast
 
 def main():
 	print(ast.literal_eval('[1,2]'))
-	print("This is print from local repo")
+	print("This is print from github again after resolving conflict")
 
 if __name__ == '__main__':
 	main()
