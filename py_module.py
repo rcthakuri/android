503857f5feaf323388e545fd8f2b810ff91a3341
Ramchandra <thakuri.ram12@gmail.com>
2022-01-08 14:21:21 +0545

    Send print() changed on main()

--- py_module.py
+++ py_module.py
@@ -2,6 +2,6 @@ import ast
 
 def main():
 	print(ast.literal_eval('[1,2]'))
-	print("This is print from github again after resolving ok conflict")
+	print("This is print from github again after resolving ok conflict ok ok")
 if __name__ == '__main__':
 	main()
